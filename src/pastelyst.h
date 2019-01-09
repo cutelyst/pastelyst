f42f9e5dfd2ce57bad1db2f2bfdf6effe2e91233
Anıl Özbek <ozbekanil@gmail.com>
2019-01-09 14:38:38 +0300

    update copyrigt dates and disable Qt5::Gui to figure out whether it really needed

--- src/pastelyst.h
+++ src/pastelyst.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2017-2018 Daniel Nicoletti <dantti12@gmail.com>         *
+ *   Copyright (C) 2017-2019 Daniel Nicoletti <dantti12@gmail.com>         *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -42,4 +42,3 @@ private:
 };
 
 #endif //PASTELYST_H
-
